25b7a2e8994fb6abd617672be39845c26dbd7c5e
Rashid Teymurlu <rasidtt999@gmail.com>
2023-05-14 20:39:41 +0400

    added main method to ArrayListProblem.java

--- src/ArrayListProblem.java
+++ src/ArrayListProblem.java
@@ -1,7 +1,11 @@
 import java.util.*;
 public class ArrayListProblem {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> integerList = getList(list,n,sc);
+        checkList(list,sc);
     }
     public static ArrayList<ArrayList<Integer>> getList(ArrayList<ArrayList<Integer>> list,int n,Scanner sc) {
 
