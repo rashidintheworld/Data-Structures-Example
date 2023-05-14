680f63f55914697d1120237e901f580ea2d1ff21
Rashid Teymurlu <rasidtt999@gmail.com>
2023-05-14 15:22:41 +0400

    added sum hour glass method to TwoDimensionArrayProblem.java

--- src/TwoDimensionArrayProblem.java
+++ src/TwoDimensionArrayProblem.java
@@ -2,7 +2,18 @@ public class TwoDimensionArrayProblem {
     public static void main(String[] args) {
 
     }
-    public int sumHourGlass(){
-        
+    public int sumHourGlass(int arr[][]){
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i <= 3; i++) {
+            for (int j = 0; j <= 3; j++) {
+                int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                        arr[i + 1][j + 1]
+                        + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                if (maxSum < sum) {
+                    maxSum = sum;
+                }
+            }
+        }
+        return maxSum;
     }
 }
